1cb915397afa4cc57499fcccd54e9917f2ccab30
mechapear <mechapear@gmail.com>
2024-02-02 15:36:30 +0700

    chore: set up the Prettier plugin for Tailwind CSS and add the Tailwind Form Plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import tailwindCssForm from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // @tailwindcss/forms - reset form styles making form elements easy to override
+  plugins: [tailwindCssForm],
 }
