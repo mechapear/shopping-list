2b21b6dc7f319478e552b0177de0bff58a6fb6b7
mechapear <mechapear@gmail.com>
2024-02-02 15:37:02 +0700

    feat: update CSS styles for the shopping list app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import DeleteIcon from '@/icons/DeleteIcon.vue'
+import ArrowRightIcon from './icons/ArrowRightIcon.vue'
+import ArrowDownIcon from './icons/ArrowDownIcon.vue'
 import { ref, computed, watch, onMounted } from 'vue'
 
 // example of shopping list categories
@@ -27,7 +30,7 @@ const itemName = ref('')
 const quantity = ref('')
 const category = ref('')
 const shoppingList = ref([])
-const showPurchasedItem = ref(false)
+const showPurchasedItem = ref(true)
 
 // Computed ref - update real time
 const unpurchasedItemList = computed(() => {
@@ -80,84 +83,151 @@ function handleDeleteItem(id) {
 </script>
 
 <template>
-  <h1>Shopping List</h1>
-  <!-- .prevent = prevent default -->
-  <form @submit.prevent="handleAddNewItem">
-    <!-- v-model.trim => remove any whitespace -->
-    <input
-      v-model.trim="itemName"
-      type="text"
-      name="itemName"
-      placeholder="add new item"
-      class="border border-gray-300"
-    />
-
-    <label>
-      <span>Quantity</span>
+  <div class="grid place-content-center gap-8 p-6">
+    <h1 class="py-3 text-left text-2xl font-semibold">Shopping List</h1>
+    <!-- .prevent = prevent default -->
+    <form @submit.prevent="handleAddNewItem" class="grid gap-5">
+      <!-- v-model.trim => remove any whitespace -->
       <input
-        v-model.trim="quantity"
+        v-model.trim="itemName"
         type="text"
-        name="quantity"
-        class="border border-gray-300"
+        name="itemName"
+        placeholder="Apple"
+        class="block w-full rounded-lg border-0 py-2 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       />
-    </label>
-    <label>Category</label>
 
-    <input
-      v-model.trim="category"
-      type="text"
-      name="category"
-      list="category"
-      placeholder="add category"
-      class="border border-gray-300"
-    />
-    <!-- Using datalist for a pre-defined options for an input element -->
-    <!-- Using index as a key because this option list won't be change -->
-    <datalist id="category">
-      <option
-        v-for="(category, index) in PRESET_CATEGORIES"
-        :value="category"
-        :key="index"
-      />
-    </datalist>
+      <div class="grid grid-cols-3 gap-4">
+        <div>
+          <label class="mb-2 block text-sm font-medium text-gray-900"
+            >Quantity</label
+          >
+          <input
+            v-model.trim="quantity"
+            type="number"
+            name="quantity"
+            placeholder="1"
+            class="block w-full rounded-lg border-0 py-2 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
 
-    <!-- Disable the button when there is no centent using v-bind -->
-    <button
-      :disabled="itemName.length === 0"
-      type="submit"
-      class="border border-gray-300"
-    >
-      Add new item
-    </button>
-  </form>
+        <div class="col-span-2">
+          <label class="mb-2 block text-sm font-medium text-gray-900"
+            >Category</label
+          >
+          <input
+            v-model.trim="category"
+            type="text"
+            name="category"
+            list="category"
+            placeholder="Fruits"
+            class="block w-full rounded-lg border-0 py-2 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <!-- Using datalist for a pre-defined options for an input element -->
+          <!-- Using index as a key because this option list won't be change -->
+          <datalist id="category">
+            <option
+              v-for="(category, index) in PRESET_CATEGORIES"
+              :value="category"
+              :key="index"
+            />
+          </datalist>
+        </div>
+      </div>
 
-  <!-- Display shopping list-->
-  <!-- Unpurchased Item List -->
-  <ul>
-    <li v-for="item in unpurchasedItemList" :key="item.id">
-      <input v-model="item.purchased" type="checkbox" />
-      <span>{{ item.name }} - {{ item.quantity }} - {{ item.category }}</span>
-      <button @click="handleDeleteItem(item.id)">Delete</button>
-    </li>
-  </ul>
+      <!-- Disable the button when there is no centent using v-bind -->
+      <button
+        :disabled="itemName.length === 0"
+        type="submit"
+        class="block w-full rounded-lg bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-not-allowed disabled:opacity-50"
+      >
+        Add new item
+      </button>
+    </form>
 
-  <!-- Purchased Item List -->
-  <div>
-    <button @click="showPurchasedItem = !showPurchasedItem" type="button">
-      {{ showPurchasedItem ? 'Hide' : 'Show' }}
-    </button>
-    <p>Purchased item</p>
+    <!-- Display shopping list-->
+    <!-- Unpurchased Item List -->
+    <ul class="divide-y divide-gray-100">
+      <li
+        v-for="item in unpurchasedItemList"
+        :key="item.id"
+        class="grid grid-cols-7 items-center py-3"
+      >
+        <div class="col-span-3 flex items-center">
+          <input
+            v-model="item.purchased"
+            type="checkbox"
+            class="mr-2 rounded-full border-gray-300 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0 md:mr-3"
+          />
+          <p>
+            {{ item.name }}
+            <span class="text-sm text-gray-500">x {{ item.quantity }}</span>
+          </p>
+        </div>
+        <span
+          class="col-span-3 w-fit rounded bg-indigo-50 px-1.5 py-0.5 text-xs text-indigo-800"
+        >
+          {{ item.category }}</span
+        >
+        <div
+          class="mx-auto flex h-7 w-7 justify-center rounded-full hover:bg-gray-100"
+        >
+          <button @click="handleDeleteItem(item.id)" class="rounded-full">
+            <DeleteIcon />
+          </button>
+        </div>
+      </li>
+    </ul>
+
+    <!-- Purchased Item List -->
+    <section>
+      <div
+        class="border-y border-gray-100 py-4 text-gray-400 hover:text-gray-900"
+      >
+        <button
+          @click="showPurchasedItem = !showPurchasedItem"
+          type="button"
+          class="w-full text-start"
+        >
+          <ArrowDownIcon v-if="showPurchasedItem" />
+          <ArrowRightIcon v-else />
+          Purchased <span class="font-medium">{{ countPurchased }}</span> of
+          <span class="font-medium">{{ countAll }}</span> items
+        </button>
+      </div>
+
+      <!-- Hide or show the purchased item list depends on the value of showPurchasedItem -->
+      <!-- If it is true, display the list -->
+      <ul v-if="showPurchasedItem" class="divide-y divide-gray-100">
+        <li
+          v-for="item in purchasedItemList"
+          :key="item.id"
+          class="grid grid-cols-7 items-center py-3"
+        >
+          <div class="col-span-3 flex items-center">
+            <input
+              v-model="item.purchased"
+              type="checkbox"
+              class="mr-2 rounded-full border-gray-300 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0 md:mr-3"
+            />
+            <p class="text-gray-400 line-through">
+              {{ item.name }}
+              <span class="text-gray-300">x {{ item.quantity }}</span>
+            </p>
+          </div>
+          <span
+            class="col-span-3 w-fit rounded bg-indigo-50 px-1.5 py-0.5 text-xs text-indigo-800"
+          >
+            {{ item.category }}</span
+          >
+          <div
+            class="mx-auto flex h-7 w-7 justify-center rounded-full hover:bg-gray-100"
+          >
+            <button @click="handleDeleteItem(item.id)" class="rounded-full">
+              <DeleteIcon />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
-
-  <!-- Hide or show the purchased item list depends on the value of showPurchasedItem -->
-  <!-- If it is true, display the list -->
-  <ul v-if="showPurchasedItem">
-    <li v-for="item in purchasedItemList" :key="item.id">
-      <input v-model="item.purchased" type="checkbox" />
-      <span>{{ item.name }} - {{ item.quantity }} - {{ item.category }}</span>
-      <button @click="handleDeleteItem(item.id)">Delete</button>
-    </li>
-  </ul>
-
-  <p>Purchased {{ countPurchased }} of {{ countAll }} items</p>
 </template>
